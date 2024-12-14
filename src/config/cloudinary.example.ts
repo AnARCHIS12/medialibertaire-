312388c0c9d9b37f80dea0a4c29c463371225c49
AnARCHIS12 <fakachampionship@gmail.com>
2024-12-14 01:54:40 +0100

    configcloudnary

---
+++ src/config/cloudinary.example.ts
@@ -0,0 +1,6 @@
+// Renommez ce fichier en cloudinary.ts et mettez à jour les valeurs
+export const CLOUDINARY_CONFIG = {
+  cloudName: 'votre_cloud_name',
+  uploadPreset: 'votre_upload_preset',
+  folder: 'medialiber/avatars'
+};
--- src/config/cloudinary.ts
+++
@@ -1,5 +0,0 @@
-export const CLOUDINARY_CONFIG = {
-  cloudName: 'debynh32n',
-  uploadPreset: 'medialiber_preset',
-  folder: 'medialiber'
-};
